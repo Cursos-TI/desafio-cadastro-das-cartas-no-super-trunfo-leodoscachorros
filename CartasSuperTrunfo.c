4704b7090795e1d923efc86976701cf7a1cebd9f
leodoscachorros <198642582+leodoscachorros@users.noreply.github.com>
2025-03-07 00:20:42 +0000

    criei printf estado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 
-int main() {
-    printf("desafio super trunfo!\n");    
+int main()
+{
+    printf("desafio super trunfo!\n");
     printf("novo comit\n");
 
+    char estado[50];
+    char codigodacarta;
+    char nomedacidade[50];
+    int populaçao;
+    float areaemkm²;
+    float pib;
+    int numerodepontosturisticos;
+
+    printf("digite seu estado:\n");
+     scanf("%s , &estado");
 
 
 
-
-
-
-
-
-
-
-    return 0;
-}
+        return 0
+    }
