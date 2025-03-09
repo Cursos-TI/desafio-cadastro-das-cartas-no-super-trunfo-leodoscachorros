00ff9d5123e178b377981a6fe6cc951faba92a59
leodoscachorros <198642582+leodoscachorros@users.noreply.github.com>
2025-03-09 13:44:14 +0000

    complicado

--- xadrez.c
+++ xadrez.c
@@ -14,34 +14,40 @@ int main(){
     int numero_de_pontos_turisticos;
 
     printf("Digite seu estado:\n");
-    scanf("%49s", estado);  
+    scanf("%50c", estado);  
 
     printf("Digite o código da carta:\n");
     scanf(" %c", &codigo_da_carta);  
 
     printf("Digite o nome da cidade:\n");
-    scanf("%49s", nome_da_cidade);  
+    scanf("%c", nome_da_cidade);  
 
     printf("Digite a população:\n");
     scanf("%d", &populacao);
 
     printf("Digite a área em km²:\n");
-    scanf("%f", &area_em_km2);
+    scanf("%2.f", &area_em_km2);
 
     printf("Digite o PIB:\n");
-    scanf("%f", &pib);
+    scanf("%2.f", &pib);
 
     printf("Digite o número de pontos turísticos:\n");
     scanf("%d", &numero_de_pontos_turisticos);
 
    
    
-    printf("Estado: %s\n", estado);
+    printf("Estado: %c\n", estado);
+
     printf("Código da Carta: %c\n", codigo_da_carta);
-    printf("Nome da Cidade: %s\n", nome_da_cidade);
+     
+    printf("Nome da Cidade: %c\n", nome_da_cidade);
+
     printf("População: %d\n", populacao);
-    printf("Área em km²: %.2f\n", area_em_km2);
-    printf("PIB: %.2f\n", pib);
+
+    printf("Área em km²: %f\n", area_em_km2);
+
+    printf("PIB: %f\n", pib);
+
     printf("Número de Pontos Turísticos: %d\n", numero_de_pontos_turisticos);
 
     return 0;
