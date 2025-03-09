e0f878345e64ad79a2451c99fba5cca199a79888
leodoscachorros <198642582+leodoscachorros@users.noreply.github.com>
2025-03-09 15:34:10 +0000

    desafio pronto parte 1(uma carta)

--- xadrez.c
+++ xadrez.c
@@ -5,7 +5,7 @@ int main() {
     printf("Novo Comit\n");
 
     char estado[51];  // Alocando espaço para a string estado
-    char codigo_da_carta;
+    char codigo_da_carta[10];
     char nome_da_cidade[100];  // Alocando espaço para a string nome_da_cidade
     int populacao;
     float area_em_km2;
@@ -16,7 +16,7 @@ int main() {
     scanf("%50s", estado);  // Usando %50s para ler uma string com até 50 caracteres
 
     printf("Digite o código da carta:\n");
-    scanf(" %c", &codigo_da_carta);  
+    scanf("%9s", codigo_da_carta);  
 
     printf("Digite o nome da cidade:\n");
     scanf("%99s", nome_da_cidade);  // Usando %99s para ler uma string com até 99 caracteres
@@ -34,7 +34,7 @@ int main() {
     scanf("%d", &numero_de_pontos_turisticos);
 
     printf("Estado: %s\n", estado);  // Usando %s para imprimir uma string
-    printf("Código da carta: %c\n", codigo_da_carta);
+    printf("Código da carta: %s\n", codigo_da_carta);
     printf("Nome da cidade: %s\n", nome_da_cidade);
     printf("População: %d\n", populacao);
     printf("Área em km²: %.2f\n", area_em_km2);  // Usando %.2f para imprimir um valor de ponto flutuante com 2 casas decimais
