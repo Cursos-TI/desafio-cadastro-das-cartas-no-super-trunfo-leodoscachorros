136ea036b7e0e0c23e3c7410f2f688882e6c4328
leodoscachorros <198642582+leodoscachorros@users.noreply.github.com>
2025-03-09 15:52:02 +0000

    PRIMEIRA CARTA PRONTA

--- xadrez.c
+++ xadrez.c
@@ -4,22 +4,22 @@ int main() {
     printf("Desafio Xaderz\n");
     printf("Novo Comit\n");
 
-    char estado[51];  // Alocando espaço para a string estado
+    char estado[51]; 
     char codigo_da_carta[10];
-    char nome_da_cidade[100];  // Alocando espaço para a string nome_da_cidade
+    char nome_da_cidade[100];  
     int populacao;
     float area_em_km2;
     float pib;
     int numero_de_pontos_turisticos;
 
     printf("Digite o estado:\n");
-    scanf("%50s", estado);  // Usando %50s para ler uma string com até 50 caracteres
+    scanf("%50s", estado);  
 
     printf("Digite o código da carta:\n");
     scanf("%9s", codigo_da_carta);  
 
     printf("Digite o nome da cidade:\n");
-    scanf("%99s", nome_da_cidade);  // Usando %99s para ler uma string com até 99 caracteres
+    scanf("%99s", nome_da_cidade);  
 
     printf("Digite a população:\n");
     scanf("%d", &populacao);
@@ -28,18 +28,19 @@ int main() {
     scanf("%f", &area_em_km2);
 
     printf("Digite o PIB:\n");
-    scanf("%f", &pib);  // Usando %f para ler um valor de ponto flutuante
+    scanf("%f", &pib);
 
     printf("Digite o número de pontos turísticos:\n");
     scanf("%d", &numero_de_pontos_turisticos);
 
-    printf("Estado: %s\n", estado);  // Usando %s para imprimir uma string
+    printf("Estado: %s\n", estado);  
     printf("Código da carta: %s\n", codigo_da_carta);
     printf("Nome da cidade: %s\n", nome_da_cidade);
     printf("População: %d\n", populacao);
-    printf("Área em km²: %.2f\n", area_em_km2);  // Usando %.2f para imprimir um valor de ponto flutuante com 2 casas decimais
-    printf("PIB: %.2f\n", pib);  // Usando %.2f para imprimir um valor de ponto flutuante com 2 casas decimais
+    printf("Área em km²: %.2f\n", area_em_km2); 
+    printf("PIB: %.2f\n", pib);
     printf("Número de pontos turísticos: %d\n", numero_de_pontos_turisticos);
 
     return 0;
 }
+
